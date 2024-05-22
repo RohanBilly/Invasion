ff399565d3f8c9cd77f53f215dcaf85143b2a231
RohanBilly <rohan2.vansanden@live.uwe.ac.uk>
2024-05-22 23:15:55 +0100

    Added scroll bar for towers

--- Invasion/Assets/Scripts/InputManager.cs
+++ Invasion/Assets/Scripts/InputManager.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
+[DefaultExecutionOrder(-1)]
 public class InputManager : MonoBehaviour
 {
     private TouchControls TouchControls;
 
+    public delegate void StartTouchEvent(Vector2 position, float time);
+    public event StartTouchEvent OnStartTouch;
+
+    public delegate void EndTouchEvent(Vector2 position, float time);
+    public event EndTouchEvent OnEndTouch;
     private void Awake()
     {
         TouchControls = new TouchControls();
@@ -31,10 +38,14 @@ public class InputManager : MonoBehaviour
     private void StartTouch(InputAction.CallbackContext context)
     {
         print("Touch Started " + TouchControls.Touch.TouchPosition.ReadValue<Vector2>());
+        if (OnStartTouch != null) OnStartTouch(TouchControls.Touch.TouchPosition.ReadValue<Vector2>(), (float)context.startTime);
+        
+
+        
     }
 
     private void EndTouch(InputAction.CallbackContext context)
     {
-
+        if (OnEndTouch != null) OnEndTouch(TouchControls.Touch.TouchPosition.ReadValue<Vector2>(), (float)context.time);
     }
 }
